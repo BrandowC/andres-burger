3580a33317041cf3a9028e3e685a4c7e0cdc4854
BrandowC <brandowclaros@gmail.com>
2026-06-30 11:10:54 -0500

    Fix backend build and addition delete

--- apps/api/src/modules/menu/application/menu.service.ts
+++ apps/api/src/modules/menu/application/menu.service.ts
@@ -66,7 +66,7 @@ export class MenuService {
         additions: {
           where: {
             isActive: true,
-            delete: null,
+            deletedAt: null,
           },
           orderBy: {
             name: 'asc',
